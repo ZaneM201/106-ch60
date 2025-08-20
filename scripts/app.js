ba6a6ba4e0bb3c498d331cccc6f27ef44d27e9c7
Zane Moon <zanem201@gmail.com>
2025-08-19 20:19:49 -0700

    106 class 2 work

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,29 @@
-function hello(){
-    console.log("Hello there");
-    goodbye();
+let isVisible = true; // global variable to track visibiltiy
+
+function toggleVisibility(){
+    if(isVisible){
+        $("#form").fadeOut(); // hide the form section
+        isVisible = false; // update visibility state
+    }else{
+        $("#form").fadeIn(); // show the form section
+        isVisible = true; // update visibility state
+    }
 }
 
-function goodbye(){
-    console.log("Bye!");
-}
 
+function saveTask(){
+    console.log("hello Im the saveTask");
+    // get the value of the input
+
+    // create a new task object
+
+    //add the task to the list
+}
 function init(){
     console.log("hello I am the init");
-    hello();
+    // hooks
+    $("#btnAdd").click(saveTask);
+    $("#btnDetails").click(toggleVisibility);
 }
 
-window.onload = init;// It waits until the HTML and CSS finish to run the logic
+window.onload = init; // It waits until the HTML and CSS finish to run the logic
